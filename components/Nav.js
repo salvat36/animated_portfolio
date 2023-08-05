d793ad8f1652b61b393f6878b11ed6ed9b9669d5
ryan salvato <salvat36@gmail.com>
2023-08-05 11:46:23 -0500

    add nav links with hover styling

--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,59 @@
 //  links
-import { HiHome } from "react-icons/hi";
-import { HiUser } from "react-icons/hi";
-import { HiRectangleGroup } from "react-icons/hi"
-import { HiViewColumns } from "react-icons/hi"
-import { HiChatBubbleBottomCenterText } from "react-icons/hi"
-import { HiEnvelope } from "react-icons/hi"
+import { HiHome } from "react-icons/hi2";
+import { HiUser } from "react-icons/hi2";
+import { HiRectangleGroup } from "react-icons/hi2";
+import { HiViewColumns } from "react-icons/hi2";
+import { HiChatBubbleBottomCenterText } from "react-icons/hi2";
+import { HiEnvelope } from "react-icons/hi2";
 
-const links = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export const links = [
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+        {links.map((link, index) => {
+          return (
+            <Link
+              className={`${
+                link.path === pathname && "text-accent"
+              } relative flex items-center group hover:text-accent transition-all duration-300`}
+              href={link.path}
+              key={index}
+            >
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
+                <div className="bg-white relative flex text-primary items-center p-[6px]'">
+                  <div className="text-[12px]">{Link.name}</div>
+                </div>
+              </div>
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
