477956244cb0a7920022d11aba88f91fc6f45928
ryan salvato <salvat36@gmail.com>
2023-08-05 17:19:00 -0500

    create transition animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import '../styles/globals.css';
+import Transition from '../components/Transition'
 
 // local imports
 import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
+import { userAgent } from 'next/server';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
-  <Layout>
-  <Component {...pageProps} />
-  </Layout>
-  )
+    <Layout>
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className='h-full'>
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
+    </Layout>
+  );
 }
 
 export default MyApp;
