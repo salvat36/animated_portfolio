60e15b21974c3df319b8fdfda9d517c61057cbdb
ryan salvato <salvat36@gmail.com>
2023-08-05 10:46:22 -0500

    create basic navigation

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,26 @@
-const Layout = () => {
-  return <div>Layout</div>;
+
+
+// Font Import / settings
+import { Sora } from "@next/font/google";
+const sora = Sora ( {
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+})
+
+// Local Imports
+import Nav from '../components/Nav'
+import Header from '../components/Header'
+import TopLeftimg from '../components/TopLeftImg'
+
+const Layout = ( {children }) => {
+  return (
+  <div>
+    <TopLeftimg />
+    <Nav />
+    <Header />
+    {children}
+  </div>);
 };
 
 export default Layout;
--- components/Nav.js
+++ components/Nav.js
@@ -1,4 +1,11 @@
 //  links
+import { HiHome } from "react-icons/hi";
+import { HiUser } from "react-icons/hi";
+import { HiRectangleGroup } from "react-icons/hi"
+import { HiViewColumns } from "react-icons/hi"
+import { HiChatBubbleBottomCenterText } from "react-icons/hi"
+import { HiEnvelope } from "react-icons/hi"
+
 const links = [
   { name: 'home', path: '/', icon: <HiHome /> },
   { name: 'about', path: '/about', icon: <HiUser /> },
