50b332f5a2c63d67b6339dc7dabd5f2946bfbb0f
ryan salvato <salvat36@gmail.com>
2023-08-05 17:30:11 -0500

    add two additional motion div

--- components/Transition.js
+++ components/Transition.js
@@ -21,15 +21,27 @@ const Transition = () => {
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
         variants={transitionVariants}
-        initial='initial'
-        animate='animate'
-        exit='exit'
+        initial="initial"
+        animate="animate"
+        exit="exit"
         transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
-      >
-        1
-      </motion.div>
-      <div>2</div>
-      <div>3</div>
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      ></motion.div>
     </>
   );
 };
