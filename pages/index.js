47b914b3aa28a52f502f83bedd3275abd4a01e89
ryan salvato <salvat36@gmail.com>
2023-08-05 20:30:43 -0500

    correct bug with projectsbtn animation

--- pages/index.js
+++ pages/index.js
@@ -15,19 +15,39 @@ const Home = () => {
       <div>
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <h1 className="h1">
+          <motion.h1
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
+          >
             Tranforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality!</span>
-          </h1>
+          </motion.h1>
           {/* subtitle */}
-          <p className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb10 xl:mb-16">
+          <motion.p
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
             Lorem ipsum dolor shiver blop and adisciptin eitl. Itacue nostrum
             erosh vero, tenetar voluptatem nulla aut esperantor dolores ut.
-          </p>
+          </motion.p>
           {/* button */}
-          <div className="flex justify-center xl:hidden relative">
+          <div className="justify-center hidden">
             <ProjectsBtn />
           </div>
+          <motion.div
+            variants={fadeIn("down", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}
