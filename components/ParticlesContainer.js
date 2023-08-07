e570e7fb6e6cbc734c7e8a7c0ac933b76638b70b
ryan salvato <salvat36@gmail.com>
2023-08-06 23:46:40 -0500

    fix issue with particles not showing

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,4 @@
-import { m } from "framer-motion";
-import { useCallback } from "react";
+import React, { useCallback } from "react";
 import { Particles } from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
@@ -12,81 +11,82 @@ const ParticlesContainer = () => {
 
   return (
     <Particles
+      className="w-full h-full absolute translate-z-0"
       id="tsparticles"
       init={particlesInit}
       loaded={particlesLoaded}
       options={{
-        fullScreen: { enable: false },
+        fullScreen: {enable: false},
         background: {
           color: {
-            value: "",
+            value: ''
           },
         },
-        fpslimit: 120,
+        fpsLimit: 120,
         interactivity: {
           events: {
             onClick: {
               enable: false,
-              mode: "push",
+              mode: "push"
             },
             onHover: {
               enable: true,
-              mode: "repulse",
+              mode: "repulse"
             },
-            resize: true,
+            resize: true
           },
           modes: {
             push: {
-              quantity: 90,
+              quantity: 90
             },
             repulse: {
               distance: 200,
-              duration: 0.4,
-            },
-          },
+              duration: 0.4
+            }
+          }
+        },
           particles: {
             color: {
-              value: "#e68e2e",
+              value: "#e68e2e"
             },
             links: {
               color: "#f5d393",
               distance: 150,
               enable: true,
               opacity: 0.5,
-              width: 1,
+              width: 1
             },
             collisions: {
-              enable: true,
+              enable: true
             },
             move: {
               directions: "none",
               enable: true,
               outModes: {
-                default: "bounce",
+                default: "bounce"
               },
               random: false,
               speed: 1,
-              straight: false,
+              straight: false
             },
             number: {
               density: {
                 enable: true,
-                area: 800,
+                area: 800
               },
-              value: 80,
+              value: 80
             },
             opacity: {
-              value: 0.5,
+              value: 0.5
             },
             shape: {
-              type: "circle",
+              type: "circle"
             },
             size: {
               value: { min: 1, max: 5 },
-              detectRetina: true,
+              detectRetina: true
             },
           },
-        },
       }}
     />
   );
--- pages/index.js
+++ pages/index.js
@@ -61,7 +61,7 @@ const Home = () => {
           variants={fadeIn("up", 0.5)}
           initial="hidden"
           animate="show"
-          exit="hidden"
+          exit="hidden" 
           transition={{duration: 1, ease: 'easeInOut'}}
           className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
         >
