8d9b062926c511c6c8379e0afe3fca2479e3999b
ryan salvato <salvat36@gmail.com>
2023-08-07 02:43:13 -0500

    add framer motion to about/index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -8,6 +8,8 @@ import { fadeIn } from "../../variants";
 import Avatar from "../../components/Avatar";
 import Circles from "../../components/Circles";
 
+import CountUp from "react-countup";
+
 export const aboutData = [
   {
     title: "skills",
@@ -97,12 +99,60 @@ const About = () => {
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
         {/* Txt */}
         <div className="flex-1 flex flex-col justify-center">
-          <h2> Captivating <span className="text-accent">Header</span> Entered Here</h2>
-          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"> Elevator Pitch regarding the when/why/how we ended up here and what I can do to skyrocket your company if you were to be smart enough to  hire me! </p>
-
+          <motion.h2
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2"
+          >
+            {" "}
+            Captivating <span className="text-accent">Header</span> Entered Here
+          </motion.h2>
+          <motion.p
+            variants={fadeIn("right", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0"
+          >
+            {" "}
+            Elevator Pitch regarding the when/why/how we ended up here and what
+            I can do to skyrocket your company if you were to be smart enough to
+            hire me!{" "}
+          </motion.p>
+          {/* counters */}
+          <motion.div
+            variants={fadeIn("right", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0 mb-8"
+          >
+            <div className="flex flex-1 xl:gap-x-6 ">
+              {/* experience */}
+              <div
+                className="relative flex-1 after:w-[1px] after:h-full
+               after:bg-white/10 after:absolute after:top-0 after:right-0"
+              >
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={10} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Years of experience
+                </div>
+              </div>
+            </div>
+          </motion.div>
         </div>
         {/* info */}
-        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px] ">
+        <motion.div
+          variants={fadeIn("left", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="flex flex-col w-full xl:max-w-[48%] h-[480px] "
+        >
           <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
@@ -122,22 +172,29 @@ const About = () => {
           <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
             {aboutData[index].info.map((item, itemIndex) => {
               return (
-                <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 ">
+                <div
+                  key={itemIndex}
+                  className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60 "
+                >
                   {/* Titles */}
                   <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
                   <div className="hidden md:flex ">-</div>
                   <div>{item.stage}</div>
                   <div className="flex gap-x-4 ">
-                  {/* Icons */}
-                  {item.icons?.map((icon, itemIndex) => {
-                    return <div className='text-2xl text-white' key={itemIndex}>{icon}</div>
-                  })}
+                    {/* Icons */}
+                    {item.icons?.map((icon, itemIndex) => {
+                      return (
+                        <div className="text-2xl text-white" key={itemIndex}>
+                          {icon}
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
               );
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
