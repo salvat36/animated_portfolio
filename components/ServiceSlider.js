873a3fd0e7260fed7ab68b0a03194b7a39fc6f2c
ryan salvato <salvat36@gmail.com>
2023-08-07 04:18:30 -0500

    add text animations for services/index.js

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -65,17 +65,17 @@ const ServiceSlider = () => {
       {serviceData.map((item, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="bg-[rgba(65, 47, 123, 0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer">
+            <div className="bg-[rgba(65, 47, 123, 0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg-[rgba(89, 65, 169, 0.15)] transition-all duration-300 ">
               {/* icons */}
-              <div>{item.icon}</div>
+              <div className="text-4xl text-accent mb-4 ">{item.icon}</div>
               {/* title & description */}
-              <div> 
-                <div>{item.title}</div>
-                <p>{item.description}</p>
+              <div className="mb-8"> 
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal">{item.description}</p>
               </div>
               {/* arrow */}
               <div className="text-3xl">
-                <RxArrowTopRight/>
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300"/>
               </div>
             </div>
           </SwiperSlide>
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,27 +1,52 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import { fadeIn } from '../../variants'
+import { fadeIn } from "../../variants";
 
-import ServiceSlider from '../../components/ServiceSlider'
-import Bulb from '../../components/Bulb'
-import Circles from '../../components/Circles'
-
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
 
 const Services = () => {
   return (
     <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
-      <div className='container mx-auto'>
-        <div className='flex flex-col xl:flex-row gap-x-8'>
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
-          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-            <h2 className='h2 xl:mt-8'>My Technologies <span className='text-accent'>.</span></h2>
-            <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-             Quis dolor dolore consectetur labore tempor occaecat aute non enim mollit. Cupidatat enim ipsum esse laboris laborum est consectetur tempor do ut voluptate. Nulla dolore ipsum consectetur ea exercitation laborum laboris. Ea consequat dolore amet sunt nisi velit.
-            </p>
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
+              My Technologies <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              Quis dolor dolore consectetur labore tempor occaecat aute non enim
+              mollit. Cupidatat enim ipsum esse laboris laborum est consectetur
+              tempor do ut voluptate. Nulla dolore ipsum consectetur ea
+              exercitation laborum laboris. Ea consequat dolore amet sunt nisi
+              velit.
+            </motion.p>
           </div>
           {/* slider */}
-          <ServiceSlider />
+          <motion.div
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%] :"
+          >
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Bulb />
