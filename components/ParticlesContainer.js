149e68ad5759b7965d8ba6c7352a64ef63b8e960
ryan salvato <salvat36@gmail.com>
2023-08-09 13:15:18 -0500

    updated social links

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -16,10 +16,10 @@ const ParticlesContainer = () => {
       init={particlesInit}
       loaded={particlesLoaded}
       options={{
-        fullScreen: {enable: false},
+        fullScreen: { enable: false },
         background: {
           color: {
-            value: ''
+            value: "",
           },
         },
         fpsLimit: 120,
@@ -27,66 +27,66 @@ const ParticlesContainer = () => {
           events: {
             onClick: {
               enable: false,
-              mode: "push"
+              mode: "push",
             },
             onHover: {
               enable: true,
-              mode: "repulse"
+              mode: "repulse",
             },
-            resize: true
+            resize: true,
           },
           modes: {
             push: {
-              quantity: 90
+              quantity: 90,
             },
             repulse: {
               distance: 200,
-              duration: 0.4
-            }
-          }
-        },
-          particles: {
-            color: {
-              value: "#e68e2e"
-            },
-            links: {
-              color: "#f5d393",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1
-            },
-            collisions: {
-              enable: true
-            },
-            move: {
-              directions: "none",
-              enable: true,
-              outModes: {
-                default: "bounce"
-              },
-              random: false,
-              speed: 1,
-              straight: false
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800
-              },
-              value: 80
-            },
-            opacity: {
-              value: 0.5
-            },
-            shape: {
-              type: "circle"
-            },
-            size: {
-              value: { min: 1, max: 5 },
-              detectRetina: true
+              duration: 0.4,
             },
           },
+        },
+        particles: {
+          color: {
+            value: "#FFFFFF",
+          },
+          links: {
+            color: "#000000",
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+            detectRetina: true,
+          },
+        },
       }}
     />
   );
--- components/Socials.js
+++ components/Socials.js
@@ -9,13 +9,22 @@ import {
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-lg">
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href={"https://www.facebook.com/ryan.salvato/"}
+        className="hover:text-accent transition-all duration-300"
+      >
         <RiFacebookBoxLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href={"https://github.com/salvat36"}
+        className="hover:text-accent transition-all duration-300"
+      >
         <RiGithubLine />
       </Link>
-      <Link href={""} className="hover:text-accent transition-all duration-300">
+      <Link
+        href={"https://www.linkedin.com/in/ryan-salvato/"}
+        className="hover:text-accent transition-all duration-300"
+      >
         <RiLinkedinLine />
       </Link>
     </div>
