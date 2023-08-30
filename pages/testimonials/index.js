11acb1bdec7f9e107271f59c97b1d9e8e85b5244
ryan salvato <salvat36@gmail.com>
2023-08-30 02:08:14 -0500

    update work and school experience

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -4,17 +4,20 @@ const CV = () => {
       <div className="bg-gray-800 text-white py-4 text-center">
         <p className="text-sm mt-4">
           Salvat36@gmail.com | (845) 754-6125 |{" "}
-          <a href="https://github.com/yourgithub" class="text-blue-400">
+          <a href="https://github.com/yourgithub" className="text-blue-400">
             Github
           </a>{" "}
           |{" "}
-          <a href="https://linkedin.com/in/yourlinkedin" class="text-blue-400">
+          <a
+            href="https://linkedin.com/in/yourlinkedin"
+            className="text-blue-400"
+          >
             LinkedIn
           </a>
         </p>
       </div>
-      <div class="min-h-screen flex flex-col justify-start items-center">
-        <div class="w-full max-w-screen-md p-4 mt-4 overflow-x-hidden overflow-y-auto">
+      <div className="min-h-screen flex flex-col justify-start items-center">
+        <div className="w-full max-w-screen-md p-4 mt-4 overflow-x-hidden overflow-y-auto">
           <div className="mt-6">
             <h2 className="text-2xl font-semibold">FULL-STACK ENGINEER</h2>
             <p className="mt-2">
@@ -26,7 +29,7 @@ const CV = () => {
           {/* Technical Skills */}
           <div className="mt-10">
             <h2 className="text-2xl font-semibold">TECHNICAL SKILLS</h2>
-            <p class="font-semibold mt-2">
+            <p className="font-semibold mt-2">
               Javascript (ES6), React, Redux, Python, Postgres, SQL, Redux,
               HTML, CSS, Git, Framer Motion, NextJS, Figma, Flask
             </p>
@@ -99,6 +102,73 @@ const CV = () => {
               </p>
             </div>
             {/* PROFESSIONAL EXPERIENCE */}
+            <div className="mt-10 space-y-6">
+              <h2 className="text-2xl font-semibold">
+                PROFESSIONAL EXPERIENCE
+              </h2>
+              {/* JOB 1 */}
+              <div className="mt-4">
+                <h3 className="font-semibold">Family First Life, Irvine, CA</h3>
+                <p className="mt-2">
+                  Independent Life/Health Agent | August 2022 – Current
+                </p>
+                <p>
+                  Crafted custom life, accident, and health insurance plans
+                  aligned with diverse client needs.
+                </p>
+                <p>
+                  Harnessed technology to automate appointment booking,
+                  scheduling, and customer follow-ups through a personalized
+                  CRM.
+                </p>
+              </div>
+              {/* JOB 2 */}
+              <div className="mt-4">
+                <h3 className="font-semibold">loanDepot, Foothill Ranch, CA</h3>
+                <p className="mt-2">
+                  Licensed Lending Officer | February 2021 – July 2022
+                </p>
+                <p>
+                  Helped educate, explore financial benefits, and transform the
+                  lives of Homeowners in 10 different states.
+                </p>
+                <p>Funded over 110 loans in the last 3 Quarters.</p>
+              </div>
+              {/* JOB 3 */}
+              <div className="mt-4">
+                <h3 className="font-semibold">
+                  Office-1 Technologies, Cerritos, CA
+                </h3>
+                <p className="mt-2">
+                  Technical Sales Support | March 2018 – April 2020
+                </p>
+                <p>
+                  Established and maintained positive customer relationships
+                  through proactive communication and problem-solving.
+                </p>
+                <p>
+                  Managed and coordinated large-scale projects requiring various
+                  departments and multiple implementation phases.
+                </p>
+              </div>
+            </div>
+            {/* EDUCATION */}
+            <div className="mt-10 space-y-6">
+              <h2 className="text-2xl font-semibold">EDUCATION</h2>
+
+              <div className="mt-4">
+                <p>Software Engineering Certificate | August 2023</p>
+                <p>FlatIron University, NY New York</p>
+              </div>
+
+              <div className="mt-4">
+                <p>
+                  Bachelor’s Degree of Science in Business Economics | October
+                  2008
+                </p>
+                <p>State University of New York at Oneonta</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
