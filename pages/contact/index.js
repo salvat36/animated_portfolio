0519971bc3b93605ea878c1d2f91370f7c8ac182
ryan salvato <salvat36@gmail.com>
2023-09-03 00:17:29 -0500

    update contact form to include framer motion - upward

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -11,17 +11,35 @@ const Contact = () => {
     <div className=" h-full bg-primary/30">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
         <div className="flex flex-col w-full max-w-[700px]">
-          <h2 className="h2 text-center mb-12 ">
+          <motion.h2
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h2 text-center mb-12 "
+          >
             Let's <span className="text-accent"> connect. </span>
-          </h2>
-          <form className="flex-1 flex flex-col gap-6 w-full mx-auto ">
+          </motion.h2>
+          <motion.form
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="flex-1 flex flex-col gap-6 w-full mx-auto "
+          >
             <div className="flex gap-x-6 w-full">
               <input type="text" placeholder="name" className="input"></input>
               <input type="text" placeholder="email" className="input"></input>
             </div>
             <input type="text" placeholder="subject" className="input"></input>
-            <textarea placeholder="message"></textarea>
-          </form>
+            <textarea placeholder="message" className="textarea"></textarea>
+            <button className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group">
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                Let's talk
+              </span>
+              <BsArrowRight />
+            </button>
+          </motion.form>
         </div>
       </div>
     </div>
